9d0f9ffda4ddd3e9b09a9a8062bfc4a28599e147
AsherValentini <ashermvalentini@gmail.com>
2025-08-12 10:29:25 +0200

    [Final Sub]
    cpy ctr
    
    mv ctr
    
    mv assign op
    
    -> and *() overloads
    
    relase and release/aquire

--- include/m_memory.hpp
+++ include/m_memory.hpp
@@ -6,17 +6,114 @@ class SharedPtr {
 public:
 	using element_type = T;
 
+	// default ctr
 	SharedPtr () noexcept : ptr_(nullptr), refcnt_(nullptr) {}
 	explicit SharedPtr (T* p) : ptr_(p), refcnt_(nullptr) {
 		if(ptr_) {
 			refcnt_= new std::size_t(1);
 		}
 	}
+	// default dtr
+	~SharedPtr () {
+		release_();
+	}
+
+	// cpy ctr
+	SharedPtr (const SharedPtr& other)
+		: ptr_(other.ptr_), refcnt_(other.refcnt_) {
+			if(refcnt_){
+				++(*refcnt_);
+			}
+		}
+
+	// cpy assignment operator
+	SharedPtr& operator =(const SharedPtr& other) noexcept {
+		if (this == &other) return *this;
+
+		release_();
+
+		this->ptr_ = other.ptr_;
+		this->refcnt_ = other.refcnt_;
+		if (refcnt_) {
+			++(*refcnt_);
+		}
+
+		return *this;
+	}
+
+	// mv ctr
+	SharedPtr (SharedPtr&& other) noexcept
+		: ptr_(other.ptr_), refcnt_(other.refcnt_)
+	{
+		other.ptr_ = nullptr;
+		other.refcnt_ = nullptr;
+	}
+
+	// mv assignment operator
+	SharedPtr& operator=(SharedPtr&& other) noexcept {
+
+		if (this == &other) return *this;
+
+		release_();
+
+		this->ptr_= other.ptr_;
+		this->refcnt_ = other.refcnt_;
+
+		other.ptr_ = nullptr;
+		other.refcnt_ = nullptr;
+
+		return *this;
+
+	}
+
+	// operational overloads
+	T* operator->() {
+		return ptr_;
+	}
+
+	T& operator*() {
+		return *ptr_;
+	}
+
+	// reset a pointer
+	void release () {
+		release_(); // just drop
+	}
+
+	void release (T* p) {
+
+		// drop and aquire new memory
+		release_();
+		if (p) {
+			ptr_ = p;
+			refcnt_ = new std::size_t(1);
+		}
+	}
 
 	T* get() const noexcept {return ptr_; }
+
+	std::size_t use_count() const noexcept {
+		return (refcnt_)? *refcnt_ : 0;
+	}
+
+	explicit operator bool() const noexcept {
+		return ptr_ != nullptr;
+	}
 
 private:
 	T* ptr_;
 	std::size_t* refcnt_;
+
+	void release_ () {
+		if(!refcnt_)
+			return;
+		--(*refcnt_);
+		if(refcnt_ == 0) {
+			delete ptr_;
+			delete refcnt_;
+		}
+		ptr_ = nullptr;
+		refcnt_ = nullptr;
+	}
 
 };
--- tests/shared_pointer_test.cpp
+++ tests/shared_pointer_test.cpp
@@ -1,13 +1,59 @@
 #include "m_memory.hpp"
 #include <gtest/gtest.h>
 
-TEST(PtrStatus, PtrIsNull) {
+TEST(SharedPtrTest, PtrIsNull) {
     SharedPtr<int> a;
     EXPECT_EQ(a.get(), nullptr);
 }
 
-TEST(PtrStatusNotNull, PtrisNotNull) {
+TEST(SharedPtrTest, PtrisNotNull) {
     SharedPtr<int> b(new int(7));
     EXPECT_NE(b.get(), nullptr);
 }
 
+TEST(SharedPtrTest, IsCounting) {
+    SharedPtr<int> a(new int(7));
+    EXPECT_EQ(a.use_count(), 1);
+    SharedPtr<int> b(a);
+    EXPECT_EQ(a.use_count(), 2);
+    EXPECT_EQ(b.use_count(), 2);
+}
+
+TEST(SharedPtrTest, CpyAssign_SharesOwnership) {
+    SharedPtr<int> a(new int(10));
+    SharedPtr<int> b;
+
+    b=a;
+
+    EXPECT_EQ(a.use_count(), 2);
+    EXPECT_EQ(b.use_count(), 2);
+    EXPECT_EQ(b.get(), a.get());
+
+}
+
+TEST(SharedPtrTest, MoveConstructor) {
+
+    SharedPtr<int> a(new int(10));
+    auto tempPtr = a.get();
+    auto b = std::move(a);
+
+    EXPECT_EQ(a.use_count(), 0);
+    EXPECT_EQ(a.get(), nullptr);
+    EXPECT_EQ(b.use_count(), 1);
+    EXPECT_EQ(b.get(), tempPtr);
+}
+
+TEST(SharedPtrTest, DefaultRelease) {
+    SharedPtr<int> a(new int(10));
+    a.release();
+    EXPECT_EQ(a.get(), nullptr);
+    EXPECT_EQ(a.use_count(), 0);
+}
+
+TEST(SharedPtrTest, ReleaseAndAquire) {
+    SharedPtr<int> a(new int(10));
+    auto tempPtr = a.get();
+    a.release(new int(15));
+    EXPECT_NE(a.get(), tempPtr);
+    EXPECT_EQ(a.use_count(), 1);
+}
